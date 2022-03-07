4c85b2245f49511936c63a769599d7373ca05e45
andyye <63383967+0ev@users.noreply.github.com>
2022-03-07 12:00:47 +0900

    unstable

--- src/Game.js
+++ src/Game.js
@@ -1,145 +1,38 @@
-import Draggable from "react-draggable"
-import { useEffect, useRef, useState } from "react"
+import React, { useRef, useEffect } from 'react'
 
-
-function Piece() {
-
-  const a = useRef()
-  const [state,setState] = useState({x: 0, y: 0})
-
-  function handleDrag (e,ui) {
-    setState(
-      {
-        x : state.x+ui.deltaX,
-        y : state.y+ui.deltaY
-      }
-    )
+const Game = props => {
+  
+  const canvasRef = useRef(null)
+  
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = '#000000'
+    ctx.beginPath()
+    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+    ctx.fill()
   }
-
-  const handleStop = (e) => {
-    let square = document.elementsFromPoint(e.clientX,e.clientY)[1]
-    console.log(square)
-  }
-
-  return (
-    <div className="piece-main">
-      <Draggable ref={a} onStop={handleStop} onDrag={handleDrag}>
-        <div className="piece">
-          {state.x.toFixed()}
-        </div>
-      </Draggable>
-    </div>
-  )
-}
-
-function Pieces(params) {
-  return (
-    <>
-      <div className="grid">
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-        <div className="invis"></div>
-      </div>
-      <Piece/>
-    </>
-  )
-}
-
-// function Board() {
-
-//   return (
-//     <table className="board">
-//       {
-//         boardState.map(col => <tr>
-//           {col.map(a=><td>
-//             <div className="cell">
-//             {a}
-//             </div>
-//           </td>)}
-//         </tr>)
-//       }
-//     </table>
-//   )
-// }
-
-function Board() {
-
-  const first = useRef(null)
-  const last = useRef(null)
-
-
-  const handleResize = () => {
-    let a = first.current.getBoundingClientRect()
-    let b = last.current.getBoundingClientRect()
-    let aCenter = {x:(a.x+a.width/2),y:(a.y+a.height/2)}
-    let bCenter = {x:(b.x+b.width/2),y:(b.y+b.height/2)}
-    let center = {a:aCenter,b:bCenter}
-  }
-
+  
   useEffect(() => {
-    window.addEventListener('resize',handleResize)
-  })
-
-  return (
-    <div className="grid">
-      <div className="cell" style={{backgroundColor:"red"}} ref={first}></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell" style={{backgroundColor:"blue"}} ref={last}></div>
-    </div>
-  )
+    
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    let frameCount = 0
+    let animationFrameId
+    
+    //Our draw came here
+    const render = () => {
+      frameCount++
+      draw(context, frameCount)
+      animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+    
+    return () => {
+      window.cancelAnimationFrame(animationFrameId)
+    }
+  }, [draw])
+  
+  return <canvas ref={canvasRef} {...props}/>
 }
 
-function Game() {
-  return (
-    <div className="game-main">
-      <Board/>
-      <Pieces/>
-    </div>
-  );
-}
-
-export default Game;
+export default Game
---
+++ src/Test2.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Draggable from "react-draggable";
+import "./styles.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeDrags: 0,
+      deltaPosition: {
+        x: 0,
+        y: 0
+      },
+      controlledPosition: {
+        x: -400,
+        y: 200
+      }
+    };
+    this.handleDrag = this.handleDrag.bind(this);
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+    this.adjustXPos = this.adjustXPos.bind(this);
+    this.adjustYPos = this.adjustYPos.bind(this);
+    this.onControlledDrag = this.onControlledDrag.bind(this);
+    this.onControlledDragStop = this.onControlledDragStop.bind(this);
+  }
+
+  handleDrag(e, ui) {
+    const { x, y } = this.state.deltaPosition;
+    this.setState({
+      deltaPosition: {
+        x: x + ui.deltaX,
+        y: y + ui.deltaY
+      }
+    });
+  }
+
+  onStart() {
+    this.setState({ activeDrags: ++this.state.activeDrags });
+  }
+
+  onStop() {
+    this.setState({ activeDrags: --this.state.activeDrags });
+  }
+  // For controlled component
+  adjustXPos(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const { x, y } = this.state.controlledPosition;
+    this.setState({ controlledPosition: { x: x - 10, y } });
+  }
+
+  adjustYPos(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    const { controlledPosition } = this.state;
+    const { x, y } = controlledPosition;
+    this.setState({ controlledPosition: { x, y: y - 10 } });
+  }
+
+  onControlledDrag(e, position) {
+    const { x, y } = position;
+    this.setState({ controlledPosition: { x, y } });
+  }
+
+  onControlledDragStop(e, position) {
+    this.onControlledDrag(e, position);
+    this.onStop();
+  }
+  render() {
+    const dragHandlers = { onStart: this.onStart, onStop: this.onStop };
+    const { deltaPosition, controlledPosition } = this.state;
+    return (
+      <div>
+        <h1>React Draggable</h1>
+        <p>Active DragHandlers: {this.state.activeDrags}</p>
+        <p>
+          <a href="https://github.com/mzabriskie/react-draggable/blob/master/example/index.html">
+            Demo Source
+          </a>
+        </p>
+
+        <div
+          className="box"
+          style={{
+            height: "500px",
+            width: "500px",
+            position: "relative",
+            overflow: "auto",
+            padding: "0"
+          }}
+        >
+
+        </div>
+
+        <Draggable
+          position={controlledPosition}
+          {...dragHandlers}
+          onDrag={this.onControlledDrag}
+        >
+          <div className="box">
+            My position can be changed programmatically. <br />I have a drag
+            handler to sync state.
+            <p>
+              <a href="#" onClick={this.adjustXPos}>
+                Adjust x ({controlledPosition.x})
+              </a>
+            </p>
+            <p>
+              <a href="#" onClick={this.adjustYPos}>
+                Adjust y ({controlledPosition.y})
+              </a>
+            </p>
+          </div>
+        </Draggable>
+        <Draggable
+          position={controlledPosition}
+          {...dragHandlers}
+          onStop={this.onControlledDragStop}
+        >
+          <div className="box">
+            My position can be changed programmatically. <br />I have a dragStop
+            handler to sync state.
+            <p>
+              <a href="#" onClick={this.adjustXPos}>
+                Adjust x ({controlledPosition.x})
+              </a>
+            </p>
+            <p>
+              <a href="#" onClick={this.adjustYPos}>
+                Adjust y ({controlledPosition.y})
+              </a>
+            </p>
+          </div>
+        </Draggable>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
