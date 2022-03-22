3a17839a2c6bfbd8dcbe426ed2e3f0c4136cb403
andyye <63383967+0ev@users.noreply.github.com>
2022-03-22 22:51:24 +0900

    Add css for Front

--- src/Front.js
+++ src/Front.js
@@ -1,74 +1,87 @@
-import Draggable from "react-draggable"
-import { useRef, useState } from "react"
+import Draggable from "react-draggable";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import Title from './images/title.svg';
-import { BiUser } from 'react-icons/bi'
-import { MdPassword } from 'react-icons/md'
+import Title from "./images/title.svg";
+import { BiUser } from "react-icons/bi";
+import { MdPassword } from "react-icons/md";
 import { IconContext } from "react-icons/lib";
-function Front({socket}) {
-  const [log,setLog] = useState(false)
+import styles from "./css/Front.module.css";
 
-  const [inputs,setInputs] = useState({})
+function Front({ socket }) {
+  const [log, setLog] = useState(false);
+  const [inputs, setInputs] = useState({});
 
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs(values => ({...values, [name]: value}))
-  }
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(inputs)
-    socket.emit("sign_in",inputs.username,inputs.password)
-    socket.once("sign_in",(responseCode)=>(handleResponse(responseCode)))
-  }
+    console.log(inputs);
+    socket.emit("sign_in", inputs.username, inputs.password);
+    socket.once("sign_in", (responseCode) => handleResponse(responseCode));
+  };
 
-  const handleResponse = ({responseCode}) => {
-    if (responseCode==100){
-    } else if (responseCode===201) {
-      alert("username")
-    } else if (responseCode===202) {
-      alert("password")
-    } else if (responseCode===203) {
-      alert("username")
-    } else if (responseCode===204) {
-      alert("password")
+  const handleResponse = ({ responseCode }) => {
+    if (responseCode == 100) {
+    } else if (responseCode === 201) {
+      alert("username");
+    } else if (responseCode === 202) {
+      alert("password");
+    } else if (responseCode === 203) {
+      alert("username");
+    } else if (responseCode === 204) {
+      alert("password");
     }
-  }
+  };
 
-  let login = <div>
-    <IconContext.Provider value={{color:'white'}}>
-    <form onSubmit={handleSubmit}>
-      <h1>Hello!</h1>
-      <p>Enter your credentials to continue.</p>
-      
-      <div className="username">
-          <div><BiUser/></div>
-          <input type="text" name="username" value={inputs.username || ""} placeholder="Enter your username" onChange={handleChange}/>
-      </div>
+  let login = (
+    <div className="login">
+      <IconContext.Provider value={{ color: "white" }}>
+        <form onSubmit={handleSubmit}>
+          <div className="username">
+            <label className="icon">
+              <BiUser />
+            </label>
+            <input
+              type="text"
+              name="username"
+              value={inputs.username || ""}
+              placeholder="Enter your username"
+              onChange={handleChange}
+            />
+          </div>
 
-      <div className="password">
-          <label><MdPassword /></label>
-          <input type="password" name="password" value={inputs.password || ""} placeholder="Enter your password" onChange={handleChange}/>
-      </div>
-      <div style={{display:"none"}}>
-          <button type="submit">
-          </button>
-      </div>
-    </form>
-    </IconContext.Provider>
-  </div>
+          <div className="password">
+            <label className="icon">
+              <MdPassword />
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={inputs.password || ""}
+              placeholder="Enter your password"
+              onChange={handleChange}
+            />
+          </div>
+          <div style={{ display: "none" }}>
+            <button type="submit"></button>
+          </div>
+        </form>
+      </IconContext.Provider>
+    </div>
+  );
 
-  let enter = <div>
-
-  </div>
+  let enter = <div></div>;
 
   return (
     <div className="front">
       <div>
-        <img src={Title} style={{width:"70%"}} className="title" />
+        <img src={Title} style={{ width: "70%" }} className="title" />
       </div>
-      {log?enter:login}
+      {log ? enter : login}
     </div>
   );
 }
